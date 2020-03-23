54ccb6462fe37fa6cfd9e1c98766a95a686f8e22
johannes karoff <johannes@karoff.net>
2020-03-23 11:07:03 +0100

    add ObservableLike type class for converting to Observable

---
+++ colibri/src/main/scala/colibri/ObservableLike.scala
@@ -0,0 +1,28 @@
+package colibri
+
+import cats.effect.Effect
+import colibri.effect._
+import scala.concurrent.{ExecutionContext, Future}
+
+trait ObservableLike[-F[_]] {
+  def toObservable[A](source: F[A]): Observable[A]
+}
+object ObservableLike {
+  @inline def apply[F[_]](implicit like: ObservableLike[F]): ObservableLike[F] = like
+
+  def observableSource[F[_] : Source]: ObservableLike[F] = new ObservableLike[F] {
+    def toObservable[A](source: F[A]): Observable[A] = Observable.lift(source)
+  }
+
+  def observableEffect[F[_] : Effect]: ObservableLike[F] = new ObservableLike[F] {
+    def toObservable[A](effect: F[A]): Observable[A] = Observable.fromAsync(effect)
+  }
+
+  def observableSyncEffect[F[_] : RunSyncEffect]: ObservableLike[F] = new ObservableLike[F] {
+    def toObservable[A](effect: F[A]): Observable[A] = Observable.fromSync(effect)
+  }
+
+  def observableFuture(implicit ec: ExecutionContext): ObservableLike[Future] = new ObservableLike[Future] {
+    def toObservable[A](future: Future[A]): Observable[A] = Observable.fromFuture(future)
+  }
+}
