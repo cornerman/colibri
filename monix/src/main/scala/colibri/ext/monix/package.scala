36af48c6d67d3177d5c8a72f27963d1273deddb9
busti <oss@busti.cool>
2021-09-27 18:03:29 +0200

    fix one missing single char replacement

--- monix/src/main/scala/colibri/ext/monix/package.scala
+++ monix/src/main/scala/colibri/ext/monix/package.scala
@@ -55,8 +55,8 @@ package object monix {
   }
 
   implicit object monixObservableLiftSource extends LiftSource[Observable] {
-    def lift[G[_] : Source, A](source: G[A]): Observable[A] = Observable.create[A](OverflowStrategy.Unbounded) { observer =>
-      val sub = Source[G].subscribe(source)(observer)
+    def lift[H[_] : Source, A](source: H[A]): Observable[A] = Observable.create[A](OverflowStrategy.Unbounded) { observer =>
+      val sub = Source[H].subscribe(source)(observer)
       Cancelable(() => sub.cancel())
     }
   }
