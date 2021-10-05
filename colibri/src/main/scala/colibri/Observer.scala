81d8e472c5cefe6c2aa6a3af5138dd9a04c903ab
busti <oss@busti.cool>
2021-10-05 07:08:25 +0200

    wip

--- colibri/src/main/scala/colibri/Observer.scala
+++ colibri/src/main/scala/colibri/Observer.scala
@@ -1,19 +1,22 @@
 package colibri
 
+import cats.syntax.all._
+import cats.implicits._
 import cats.{MonoidK, Contravariant}
+import cats.effect.Sync
 import colibri.helpers._
 
 import scala.util.control.NonFatal
 
 trait Observer[-A] {
-  def onNext(value: A): Unit
-  def onError(error: Throwable): Unit
+  def onNext[F[_] : Sync](value: A): F[Unit]
+  def onError[F[_] : Sync](error: Throwable): F[Unit]
 }
 object Observer {
 
   object Empty extends Observer[Any] {
-    @inline def onNext(value: Any): Unit = ()
-    @inline def onError(error: Throwable): Unit = UnhandledErrorReporter.errorSubject.onNext(error)
+    @inline def onNext[F[_] : Sync](value: Any): F[Unit] = ().pure[F]
+    @inline def onError[F[_] : Sync](error: Throwable): F[Unit] = UnhandledErrorReporter.errorSubject.onNext(error)
   }
 
   @inline def empty = Empty
@@ -30,70 +33,64 @@ object Observer {
   def lift[G[_] : Sink, A](sink: G[A]): Observer[A] =  sink match {
     case sink: Observer[A@unchecked] => sink
     case _ => new Observer[A] {
-      def onNext(value: A): Unit = Sink[G].onNext(sink)(value)
-      def onError(error: Throwable): Unit = Sink[G].onError(sink)(error)
+      def onNext[F[_] : Sync](value: A): F[Unit] = Sink[G].onNext(sink)(value)
+      def onError[F[_] : Sync](error: Throwable): F[Unit] = Sink[G].onError(sink)(error)
     }
   }
 
   @inline def unsafeCreate[A](consume: A => Unit, failure: Throwable => Unit = UnhandledErrorReporter.errorSubject.onNext): Observer[A] = new Observer[A] {
-    def onNext(value: A): Unit = consume(value)
-    def onError(error: Throwable): Unit = failure(error)
+    def onNext[F[_] : Sync](value: A): F[Unit] = consume(value).pure[F]
+    def onError[F[_] : Sync](error: Throwable): F[Unit] = failure(error).pure[F]
   }
 
   def create[A](consume: A => Unit, failure: Throwable => Unit = UnhandledErrorReporter.errorSubject.onNext): Observer[A] = new Observer[A] {
-    def onNext(value: A): Unit = recovered(consume(value), onError)
-    def onError(error: Throwable): Unit = failure(error)
+    def onNext[F[_] : Sync](value: A): F[Unit] = recoveredF(consume(value).pure[F], onError)
+    def onError[F[_] : Sync](error: Throwable): F[Unit] = failure(error).pure[F]
   }
 
   @inline def combine[G[_] : Sink, A](sinks: G[A]*): Observer[A] = combineSeq(sinks)
 
   def combineSeq[G[_] : Sink, A](sinks: Seq[G[A]]): Observer[A] = new Observer[A] {
-    def onNext(value: A): Unit = sinks.foreach(Sink[G].onNext(_)(value))
-    def onError(error: Throwable): Unit = sinks.foreach(Sink[G].onError(_)(error))
+    def onNext[F[_] : Sync](value: A): F[Unit] = sinks.toList.traverse_(Sink[G].onNext(_)(value))
+    def onError[F[_] : Sync](error: Throwable): F[Unit] = sinks.toList.traverse_(Sink[G].onError(_)(error))
   }
 
   def combineVaried[GA[_] : Sink, GB[_] : Sink, A](sinkA: GA[A], sinkB: GB[A]): Observer[A] = new Observer[A] {
-    def onNext(value: A): Unit = {
-      Sink[GA].onNext(sinkA)(value)
-      Sink[GB].onNext(sinkB)(value)
-    }
-    def onError(error: Throwable): Unit = {
-      Sink[GA].onError(sinkA)(error)
-      Sink[GB].onError(sinkB)(error)
-    }
+    def onNext[F[_] : Sync](value: A): F[Unit] = Sink[GA].onNext(sinkA)(value) *> Sink[GB].onNext(sinkB)(value)
+    def onError[F[_] : Sync](error: Throwable): F[Unit] = Sink[GA].onError(sinkA)(error) *> Sink[GB].onError(sinkB)(error)
   }
 
   def contramap[G[_] : Sink, A, B](sink: G[_ >: A])(f: B => A): Observer[B] = new Observer[B] {
-    def onNext(value: B): Unit = recovered(Sink[G].onNext(sink)(f(value)), onError)
-    def onError(error: Throwable): Unit = Sink[G].onError(sink)(error)
+    def onNext[F[_] : Sync](value: B): F[Unit] = recoveredF(Sink[G].onNext(sink)(f(value)), onError)
+    def onError[F[_] : Sync](error: Throwable): F[Unit] = Sink[G].onError(sink)(error)
   }
 
   def contramapEither[G[_] : Sink, A, B](sink: G[_ >: A])(f: B => Either[Throwable, A]): Observer[B] = new Observer[B] {
-    def onNext(value: B): Unit = recovered(f(value) match {
+    def onNext[F[_] : Sync](value: B): F[Unit] = recoveredF(f(value) match {
       case Right(value) => Sink[G].onNext(sink)(value)
       case Left(error) => Sink[G].onError(sink)(error)
     }, onError)
-    def onError(error: Throwable): Unit = Sink[G].onError(sink)(error)
+    def onError[F[_] : Sync](error: Throwable): F[Unit] = Sink[G].onError(sink)(error)
   }
 
   def contramapFilter[G[_] : Sink, A, B](sink: G[_ >: A])(f: B => Option[A]): Observer[B] = new Observer[B] {
-    def onNext(value: B): Unit = recovered(f(value).foreach(Sink[G].onNext(sink)), onError)
-    def onError(error: Throwable): Unit = Sink[G].onError(sink)(error)
+    def onNext[F[_] : Sync](value: B): F[Unit] = recoveredF(f(value).traverse_(Sink[G].onNext(sink)), onError)
+    def onError[F[_] : Sync](error: Throwable): F[Unit] = Sink[G].onError(sink)(error)
   }
 
   def contracollect[G[_] : Sink, A, B](sink: G[_ >: A])(f: PartialFunction[B, A]): Observer[B] = new Observer[B] {
-    def onNext(value: B): Unit = recovered({ f.runWith(Sink[G].onNext(sink))(value); () }, onError)
-    def onError(error: Throwable): Unit = Sink[G].onError(sink)(error)
+    def onNext[F[_] : Sync](value: B): F[Unit] = recoveredF(f.lift(value).traverse_(Sink[G].onNext(sink)), onError)
+    def onError[F[_] : Sync](error: Throwable): F[Unit] = Sink[G].onError(sink)(error)
   }
 
   def contrafilter[G[_] : Sink, A](sink: G[_ >: A])(f: A => Boolean): Observer[A] = new Observer[A] {
-    def onNext(value: A): Unit = recovered(if (f(value)) Sink[G].onNext(sink)(value), onError)
-    def onError(error: Throwable): Unit = Sink[G].onError(sink)(error)
+    def onNext[F[_] : Sync](value: A): F[Unit] = recoveredF(Sink[G].onNext(sink)(value).whenA(f(value)), onError)
+    def onError[F[_] : Sync](error: Throwable): F[Unit] = Sink[G].onError(sink)(error)
   }
 
   def contramapIterable[G[_] : Sink, A, B](sink: G[_ >: A])(f: B => Iterable[A]): Observer[B] = new Observer[B] {
-    def onNext(value: B): Unit = recovered(f(value).foreach(Sink[G].onNext(sink)(_)), onError)
-    def onError(error: Throwable): Unit = Sink[G].onError(sink)(error)
+    def onNext[F[_] : Sync](value: B): F[Unit] = recoveredF(f(value).toList.traverse_(Sink[G].onNext(sink)), onError)
+    def onError[F[_] : Sync](error: Throwable): F[Unit] = Sink[G].onError(sink)(error)
   }
 
   def contraflattenIterable[G[_] : Sink, A, B](sink: G[_ >: A]): Observer[Iterable[A]] = contramapIterable(sink)(identity)
@@ -101,12 +98,12 @@ object Observer {
   //TODO return effect
   def contrascan[G[_] : Sink, A, B](sink: G[_ >: A])(seed: A)(f: (A, B) => A): Observer[B] = new Observer[B] {
     private var state = seed
-    def onNext(value: B): Unit = recovered({
+    def onNext[F[_] : Sync](value: B): F[Unit] = recoveredF({
       val result = f(state, value)
       state = result
       Sink[G].onNext(sink)(result)
     }, onError)
-    def onError(error: Throwable): Unit = Sink[G].onError(sink)(error)
+    def onError[F[_] : Sync](error: Throwable): F[Unit] = Sink[G].onError(sink)(error)
   }
 
   def doOnNext[G[_] : Sink, A](sink: G[_ >: A])(f: A => Unit): Observer[A] = new Observer[A] {
@@ -159,4 +156,5 @@ object Observer {
   }
 
   private def recovered(action: => Unit, onError: Throwable => Unit): Unit = try action catch { case NonFatal(t) => onError(t) }
+  private def recoveredF[F[_] : Sync](action: => F[Unit], onError: Throwable => F[Unit]): F[Unit] = try action catch { case NonFatal(t) => onError(t) }
 }
