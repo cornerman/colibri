ca5f6076a88de66304eec1730473a40e0af3c4d9
johannes karoff <johannes@karoff.net>
2022-03-30 09:53:15 +0200

    ObservableLike instance for Resource

--- colibri/src/main/scala/colibri/ObservableLike.scala
+++ colibri/src/main/scala/colibri/ObservableLike.scala
@@ -1,5 +1,6 @@
 package colibri
 
+import cats.effect.{Sync, Resource}
 import colibri.effect._
 import scala.concurrent.Future
 
@@ -13,11 +14,15 @@ object ObservableLike       {
     def toObservable[A](source: H[A]): Observable[A] = Observable.lift(source)
   }
 
-  implicit def observableSyncEffect[F[_]: RunEffect]: ObservableLike[F] = new ObservableLike[F] {
+  implicit def observableEffect[F[_]: RunEffect]: ObservableLike[F] = new ObservableLike[F] {
     def toObservable[A](effect: F[A]): Observable[A] = Observable.fromEffect(effect)
   }
 
   implicit val observableFuture: ObservableLike[Future] = new ObservableLike[Future] {
     def toObservable[A](future: Future[A]): Observable[A] = Observable.fromFuture(future)
+  }
+
+  implicit def observableResource[F[_]: RunEffect: Sync]: ObservableLike[Resource[F, *]] = new ObservableLike[Resource[F, *]] {
+    def toObservable[A](effect: Resource[F, A]): Observable[A] = Observable.fromResource(effect)
   }
 }
