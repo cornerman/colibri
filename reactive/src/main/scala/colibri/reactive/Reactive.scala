844c30e4bebe5f8f13950f8e6575204dc9269575
busti <moritz.bust@priceloop.ai>
2022-08-09 17:37:32 +0200

    remove debug prints

--- reactive/src/main/scala/colibri/reactive/Reactive.scala
+++ reactive/src/main/scala/colibri/reactive/Reactive.scala
@@ -170,7 +170,6 @@ object Var {
                 {
                   case (Some(prev @ _), Some(next)) =>
                     cache.get.set(next)
-                    println(("next: ", next, cache.toString))
 
                   case (Some(prev @ _), None) =>
                     cache = None
