a834b283f604026912ab0c083b29114617301de8
johannes karoff <johannes@karoff.net>
2022-11-22 13:14:12 +0100

    add more recative tests

--- reactive/src/test/scala/colibri/ReactiveSpec.scala
+++ reactive/src/test/scala/colibri/ReactiveSpec.scala
@@ -180,6 +180,110 @@ class ReactiveSpec extends AsyncFlatSpec with Matchers {
     received1 shouldBe List(12, 9, 6, 3, 2)
   }).unsafeRunSync()
 
+  it should "nested owners 2" in Owned.function(ownedOwner => SyncIO {
+    var received1 = List.empty[Int]
+    var innerRx   = List.empty[Int]
+    var outerRx   = List.empty[Int]
+
+    val variable  = Var(1)
+    val variable2 = Var(2)
+
+    implicit val owner: Owner = ownedOwner
+
+    def test(x: Int)(implicit owner: Owner) = Rx {
+      innerRx ::= x
+      variable2() * x
+    }
+
+    val rx = Rx {
+      val curr   = variable()
+      outerRx ::= curr
+      val result = test(curr)
+      result()
+    }
+
+    innerRx shouldBe List(1)
+    outerRx shouldBe List(1)
+    received1 shouldBe List.empty
+
+    rx.foreach(received1 ::= _)
+
+    innerRx shouldBe List(1)
+    outerRx shouldBe List(1)
+    received1 shouldBe List(2)
+
+    variable2.set(3)
+
+    innerRx shouldBe List(1, 1, 1) //TODO: triggering too often
+    outerRx shouldBe List(1, 1)
+    received1 shouldBe List(3, 2)
+
+    variable.set(2)
+
+    innerRx shouldBe List(2, 1, 1, 1)
+    outerRx shouldBe List(2, 1, 1)
+    received1 shouldBe List(6, 3, 2)
+
+    variable.set(3)
+
+    innerRx shouldBe List(3, 2, 1, 1, 1)
+    outerRx shouldBe List(3, 2, 1, 1)
+    received1 shouldBe List(9, 6, 3, 2)
+
+    variable2.set(4)
+
+    innerRx shouldBe List(3, 3, 3, 2, 1, 1, 1)  //TODO: triggering too often
+    outerRx shouldBe List(3, 3, 2, 1, 1)
+    received1 shouldBe List(12, 9, 6, 3, 2)
+  }).unsafeRunSync()
+
+  it should "sequence with nesting" in Owned(SyncIO {
+    var received1 = List.empty[Int]
+    var mapped   = List.empty[Boolean]
+
+    val variable  = Var[Option[Int]](None)
+
+    val stream = variable.sequence.switchMap { option =>
+      mapped ::= option.isDefined
+
+      option match {
+        case Some(rx) =>
+          val isOdd = rx.map(_ % 2 != 0)
+          val isEven = rx.map(_ % 2 == 0)
+
+          isEven.switchMap { isEven =>
+            isOdd.map { isOdd =>
+              if (isEven && !isOdd) 1
+              else if (!isEven && isOdd) 2
+              else if (isEven && isOdd) 11
+              else if (!isEven && !isOdd) 12
+              else 0
+            }
+          }
+
+        case None => Rx.const(-1)
+      }
+    }
+
+    mapped shouldBe List(false)
+    received1 shouldBe List.empty
+
+    stream.foreach(received1 ::= _)
+
+    mapped shouldBe List(false)
+    received1 shouldBe List(-1)
+
+    variable.set(Some(1))
+
+    mapped shouldBe List(true, false)
+    received1 shouldBe List(2, -1)
+
+    variable.set(Some(2))
+
+    mapped shouldBe List(true, false)
+    received1 shouldBe List(1, 12, 2, -1)
+  }).unsafeRunSync()
+
   it should "be distinct" in Owned(SyncIO {
     var mapped    = List.empty[Int]
     var received1 = List.empty[Boolean]
